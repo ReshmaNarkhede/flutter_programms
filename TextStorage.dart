2db88cdc84c435b4e06b2a6fdd525aeed2589f5b
ReshmaNarkhede <narkhedereshma@gmail.com>
2018-07-30 05:50:42 -0500

    Scan qr code and save url in file.
    Scan qr code and get url after scanning.
    create file in directory of mobile and save that url as text in that file.

---
+++ TextStorage.dart
@@ -0,0 +1,36 @@
+import 'package:path_provider/path_provider.dart';
+import 'dart:async';
+import 'dart:io';
+
+class TextStorage {
+  Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+    return directory.path;
+  }
+
+  Future<File> get _localFile async {
+    final path = await _localPath;
+    return File('$path/test.txt');
+  }
+
+  Future<String> readFile() async {
+    try {
+      final file = await _localFile;
+
+      String content = await file.readAsString();
+      return content;
+    } catch (e) {
+      return '';
+    }
+  }
+
+  Future<File> writeFile(String text) async {
+    final file = await _localFile;
+    return file.writeAsString('$text\r\n', mode: FileMode.append);
+  }
+
+  Future<File> cleanFile() async {
+    final file = await _localFile;
+    return file.writeAsString('');
+  }
+}
